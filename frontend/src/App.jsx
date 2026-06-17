46390f781f57b7eb00252153bcabf3dfecccdf25
Abhishek Kumar Chourasiya <abhishekchou5498@gmail.com>
2026-06-17 14:26:40 +0530

    Initial Build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+    <div>AI BlOGS</div>
+    <ol>
+      <li>AI in Education</li>
+      <li>AI in Medical</li>
+      <li>AI in Technology</li>
+      <li>AI in Finance</li>
+      <li>AI in Editing</li>
+    </ol>
+    </>
+  )
+}
+
+export default App
