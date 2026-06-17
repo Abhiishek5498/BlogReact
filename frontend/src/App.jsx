acdd8bd9b65623894dee2893ba86bedba752026d
Abhishek Kumar Chourasiya <abhishekchou5498@gmail.com>
2026-06-17 15:18:25 +0530

    Added contact form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,22 @@ export const App = () => {
       <li>AI in Finance</li>
       <li>AI in Editing</li>
     </ol>
+    <form>
+    <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required/>
+    <br/>
+     <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required/>
+    <br/>
+    <br/>
+     <label for="message">Message:</label>
+    <textarea type="text" id="name" name="name" required/>
+    <br/>
+    
+    <button type ="submit">Submit</button>
+  </div>
+    </form>
     </>
   )
 }
